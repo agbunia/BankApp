a1341bcef8247d499ac522862096991060640623
Agnieszka Bunia <agabuuuunia@gmail.com>
2022-04-26 18:48:01 +0200

    Adding a new method: create account

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Account {
 
     private String name;
     private String surname;
     private String accNumber;
     private long balance;
+
+    Scanner scanner = new Scanner(System.in);
 
     public String getName(){
         return name;
@@ -23,5 +27,26 @@ public class Account {
     public void setAccNumber(String accNumber){
         this.accNumber = accNumber;
     }
+    public long getBalance(){
+        return balance;
+    }
+    public void setBalance(long balance){
+        this.balance = balance;
+    }
+
+    public void createAccount(){
+        System.out.println("Enter your name");
+        name = scanner.next();
+
+        System.out.println("Enter your surname");
+        surname = scanner.next();
+
+        System.out.println("Enter your account number");
+        accNumber = scanner.next();
+
+        System.out.println("Enter your balance");
+        balance = scanner.nextLong();
+    }
+
 
 }
--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class BankApp {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        Account account = new Account();
+
+        account.createAccount();
 
     }
 }
